595782d7d9cd4fdf5fb5c363662a7b1f7bb734d0
João Rios <101368524+joaor1os@users.noreply.github.com>
2022-03-24 19:11:48 +0000

    new version

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask('__main__')
+app = Flask('__name__')
+
 
 @app.route('/')
 def index():
-  return render_template ('index.html')
+    return render_template('/index.html')
+
 
 @app.route('/unifran')
 def unifran():
-  return render_template ('unifran.html')
+    return render_template('unifran/unifran.html')
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+
+if __name__ == '__name__':
+    app.run(host='0.0.0.0')
